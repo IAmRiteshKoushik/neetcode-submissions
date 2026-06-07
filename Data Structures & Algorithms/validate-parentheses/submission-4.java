085f5d1def2194dc827ad34517dc4d08b95b1bed
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-06-07 19:39:18 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        for(char c: s.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+                continue;
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+            }
+
+            if (c == ')' && stack.peek() != '(') {
+                return false;
+            }
+            if (c == '}' && stack.peek() != '{') {
+                return false;
+            } 
+            if (c == ']' && stack.peek() != '[') {
+                return false;
+            }
+
+            stack.pop();
+        }
+
+        return stack.isEmpty();
+    }
+}
